c010a095f9c6ae8d7f6cc07ffc9a951abdc298ce
Kolja Wilcke <kolja@apfel.de>
2024-11-12 13:25:43 +0100

    bring back authors and tags endpoints

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use actix_web::{web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder};
-use actix_web::http::header::{ContentDisposition, DispositionType};
 use actix_files as fs;
+use actix_web::http::header::{ContentDisposition, DispositionType};
+use actix_web::{web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder};
 use authorized::Authorized;
-use std::sync::{Arc, Mutex};
+use rusqlite::{params, Connection};
 use serde_derive::Serialize;
+use std::sync::{Arc, Mutex};
 use tera::Tera;
-use rusqlite::{params, Connection}; // Result?
 
 #[macro_use]
 extern crate lazy_static;
@@ -26,6 +26,18 @@ struct Book {
     author_name: String,
 }
 
+#[derive(Debug, Serialize)]
+struct Author {
+    id: i32,
+    name: String,
+}
+
+#[derive(Debug, Serialize)]
+struct Tag {
+    id: i32,
+    name: String,
+}
+
 fn render_template(template: &Tera, name: &str, ctx: tera::Context) -> impl Responder {
     match template.render(name, &ctx) {
         Ok(body) => Ok::<_, Error>(
@@ -42,24 +54,28 @@ fn render_template(template: &Tera, name: &str, ctx: tera::Context) -> impl Resp
     }
 }
 #[actix_web::get("/cover/{id}")]
-async fn cover(data: web::Data<AppState>, image_id: web::Path<i32>, _auth: Authorized, _req: HttpRequest) -> Result<fs::NamedFile, Error> {
-
+async fn cover(
+    data: web::Data<AppState>,
+    image_id: web::Path<i32>,
+    _auth: Authorized,
+    _req: HttpRequest,
+) -> Result<fs::NamedFile, Error> {
     let db_lock = data.db.lock().unwrap();
     let image_id = image_id.into_inner();
 
-    let mut stmt = db_lock.prepare(
-        "SELECT books.path FROM books WHERE books.id = ?1 AND books.has_cover = true;"
-    ).expect("Error preparing SQL statement");
+    let mut stmt = db_lock
+        .prepare("SELECT books.path FROM books WHERE books.id = ?1 AND books.has_cover = true;")
+        .expect("Error preparing SQL statement");
 
-    let path: String = stmt.query_row(rusqlite::params![image_id], |row| {
-        row.get(0)
-    }).expect("Error retrieving image path");
+    let path: String = stmt
+        .query_row(rusqlite::params![image_id], |row| row.get(0))
+        .expect("Error retrieving image path");
 
     let cover_path = format!("{}/{}/cover.jpg", data.config.calibre.library_path, path);
 
     let file = fs::NamedFile::open(&cover_path)
         .map_err(|e| actix_web::error::ErrorInternalServerError(e))?;
-    // let file = fs::NamedFile::open(cover_path)?;
+
     Ok(file
         .use_last_modified(true)
         .set_content_disposition(ContentDisposition {
@@ -83,25 +99,38 @@ async fn opds(data: web::Data<AppState>, _auth: Authorized, _req: HttpRequest) -
     render_template(&data.templates, "index.xml.tera", ctx)
 }
 
-//#[actix_web::get("/tags")]
-//async fn gettags(data: web::Data<AppState>, _auth: Authorized, _req: HttpRequest) -> impl Responder {
-//
-//    let mut ctx = tera::Context::new();
-//
-//    ctx.insert("config", &data.config);
-//    ctx.insert(
-//        "base_url",
-//        &format!(
-//            "http://{}:{}",
-//            &data.config.server.ip, &data.config.server.port
-//        ),
-//    );
-//
-//    let db = &*data.db;
-//    let tags: Vec<calibre::tags::Model> = Tag::Entity::find().all(db).await.unwrap();
-//    ctx.insert("tags", &tags);
-//    render_template(&data.templates, "tags.xml.tera", ctx)
-//}
+#[actix_web::get("/tags")]
+async fn tags(data: web::Data<AppState>, _auth: Authorized, _req: HttpRequest) -> impl Responder {
+    let mut ctx = tera::Context::new();
+
+    ctx.insert("config", &data.config);
+    ctx.insert(
+        "base_url",
+        &format!(
+            "http://{}:{}",
+            &data.config.server.ip, &data.config.server.port
+        ),
+    );
+
+    let db_lock = data.db.lock().unwrap();
+
+    let mut stmt = db_lock
+        .prepare("SELECT id, name FROM tags;")
+        .expect("Error preparing statement");
+
+    let books_iter = stmt
+        .query_map(params![], |row| {
+            Ok(Tag {
+                id: row.get(0)?,
+                name: row.get(1)?,
+            })
+        })
+        .expect("Error querying books");
+
+    let tags: Vec<Tag> = books_iter.map(|b| b.unwrap()).collect();
+    ctx.insert("tags", &tags);
+    render_template(&data.templates, "tags.xml.tera", ctx)
+}
 
 //#[actix_web::get("/tags/{id}")]
 //async fn books_by_tag(data: web::Data<AppState>, tag_id: web::Path<i32>, _auth: Authorized, _req: HttpRequest) -> impl Responder {
@@ -126,28 +155,12 @@ async fn opds(data: web::Data<AppState>, _auth: Authorized, _req: HttpRequest) -
 //    render_template(&data.templates, "books.xml.tera", ctx)
 //}
 
-//#[actix_web::get("/authors")]
-//async fn authors(data: web::Data<AppState>, _auth: Authorized, _req: HttpRequest) -> impl Responder {
-//    let mut ctx = tera::Context::new();
-//
-//    ctx.insert("config", &data.config);
-//    ctx.insert(
-//        "base_url",
-//        &format!(
-//            "http://{}:{}",
-//            &data.config.server.ip, &data.config.server.port
-//        ),
-//    );
-//
-//    let db = &*data.db;
-//    let authors: Vec<calibre::authors::Model> = Author::find().all(db).await.unwrap();
-//    ctx.insert("authors", &authors);
-//
-//    render_template(&data.templates, "authors.xml.tera", ctx)
-//}
-
-#[actix_web::get("/books")]
-async fn getbooks(data: web::Data<AppState>, _auth: Authorized, _req: HttpRequest) -> impl Responder {
+#[actix_web::get("/authors")]
+async fn authors(
+    data: web::Data<AppState>,
+    _auth: Authorized,
+    _req: HttpRequest,
+) -> impl Responder {
     let mut ctx = tera::Context::new();
 
     ctx.insert("config", &data.config);
@@ -160,7 +173,44 @@ async fn getbooks(data: web::Data<AppState>, _auth: Authorized, _req: HttpReques
     );
 
     let db_lock = data.db.lock().unwrap();
-    // let db = &*data.db;
+
+    let mut stmt = db_lock
+        .prepare("SELECT id, name FROM authors;")
+        .expect("Error preparing statement");
+
+    let books_iter = stmt
+        .query_map(params![], |row| {
+            Ok(Author {
+                id: row.get(0)?,
+                name: row.get(1)?,
+            })
+        })
+        .expect("Error querying books");
+    let authors: Vec<Author> = books_iter.map(|b| b.unwrap()).collect();
+    ctx.insert("authors", &authors);
+
+    render_template(&data.templates, "authors.xml.tera", ctx)
+}
+
+#[actix_web::get("/books")]
+async fn getbooks(
+    data: web::Data<AppState>,
+    _auth: Authorized,
+    _req: HttpRequest,
+) -> impl Responder {
+    let mut ctx = tera::Context::new();
+
+    ctx.insert("config", &data.config);
+    ctx.insert(
+        "base_url",
+        &format!(
+            "http://{}:{}",
+            &data.config.server.ip, &data.config.server.port
+        ),
+    );
+
+    let db_lock = data.db.lock().unwrap();
+
     let mut stmt = db_lock.prepare(
         "SELECT b.id, b.title, b.pubdate, c.text AS synopsis, a.name AS author_name, a.id AS author_id
         FROM books b
@@ -169,18 +219,20 @@ async fn getbooks(data: web::Data<AppState>, _auth: Authorized, _req: HttpReques
         LEFT JOIN comments c ON b.id = c.book"
     ).expect("Error preparing statement");
 
-    let books_iter = stmt.query_map(params![], |row| {
-        Ok(Book {
-            id: row.get(0)?,
-            title: row.get(1)?,
-            pubdate: row.get(2)?,
-            synopsis: row.get(3)?,
-            author_name: row.get(4)?,
-            author_id: row.get(5)?,
+    let books_iter = stmt
+        .query_map(params![], |row| {
+            Ok(Book {
+                id: row.get(0)?,
+                title: row.get(1)?,
+                pubdate: row.get(2)?,
+                synopsis: row.get(3)?,
+                author_name: row.get(4)?,
+                author_id: row.get(5)?,
+            })
         })
-    }).expect("Error querying books");
+        .expect("Error querying books");
     let books: Vec<Book> = books_iter.map(|b| b.unwrap()).collect();
-    // let books: Vec<calibre::books::Model> = Book::find().all(db).await.unwrap();
+
     ctx.insert("books", &books);
 
     render_template(&data.templates, "books.xml.tera", ctx)
@@ -216,8 +268,8 @@ async fn main() -> std::io::Result<()> {
 
 fn init(cfg: &mut web::ServiceConfig) {
     cfg.service(opds);
-    // cfg.service(gettags);
-    // cfg.service(authors);
+    cfg.service(tags);
+    cfg.service(authors);
     cfg.service(getbooks);
     cfg.service(cover);
     // cfg.service(books_by_tag);
